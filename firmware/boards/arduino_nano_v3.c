84cb242175762f95251d3ca1c7daf3e5ed9be130
Alban Bedel <albeu@free.fr>
2017-09-11 23:21:33 +0200

    firmware: boards: nano v3: Set the config for carrier rev 0
    We now finally have the "real" carrier with true PCB, however these
    have a slightly different pinning than the prototypes build on
    perfboards. To handle this correctly we would need to support
    different board revisions. Not a big deal but as we only used arduino
    nano v2 on the prototypes, and the new devices all get arduino nano v3,
    we just use this to differentiate the hardware versions for now.
    
    Signed-off-by: Alban Bedel <albeu@free.fr>

--- firmware/boards/arduino_nano_v3.c
+++ firmware/boards/arduino_nano_v3.c
@@ -1,2 +1,33 @@
-/* The V2 and V3 have the same pinout */
-#include "arduino_nano_v2.c"
+#include <avr/pgmspace.h>
+#include "door-controller.h"
+#include "external-irq.h"
+#include "gpio.h"
+
+const struct door_ctrl_config doors_config[] PROGMEM = {
+	{
+		.door_id = 0,
+		.d0_irq = IRQ(PC, 4),
+		.d1_irq = IRQ(PC, 3),
+		.open_gpio = GPIO(C, 1, HIGH_ACTIVE),
+		.open_time = 4000,
+		.led_gpio = GPIO(B, 1, HIGH_ACTIVE),
+		.buzzer_gpio = GPIO(B, 0, HIGH_ACTIVE),
+		.status_gpio = GPIO(B, 2, LOW_ACTIVE),
+		.status_pull = 1,
+		.open_btn_gpio = GPIO(D, 7, LOW_ACTIVE),
+		.open_btn_pull = 1,
+	},
+	{
+		.door_id = 1,
+		.d0_irq = IRQ(PC, 22),
+		.d1_irq = IRQ(PC, 21),
+		.open_gpio = GPIO(C, 2, HIGH_ACTIVE),
+		.open_time = 4000,
+		.led_gpio = GPIO(D, 3, HIGH_ACTIVE),
+		.buzzer_gpio = GPIO(D, 2, HIGH_ACTIVE),
+		.status_gpio = GPIO(D, 4, LOW_ACTIVE),
+		.status_pull = 1,
+		.open_btn_gpio = GPIO(C, 0, LOW_ACTIVE),
+		.open_btn_pull = 1,
+	}
+};
